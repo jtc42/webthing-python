81765f4596df166186c8408f44d418d8f1e5466f
Joel Collins <joel@jtcollins.net>
2020-09-01 15:47:42 +0100

    Updated example

--- example/single-thing.py
+++ example/single-thing.py
@@ -1,23 +1,14 @@
 from __future__ import division
-from webthing import Action, Event, Property, SingleThing, Thing, Value, WebThingServer
+from webthing import Action, Event, Property, Thing, WebThingServer
 import logging
 import time
 import uuid
+import asyncio
 
 
 class OverheatedEvent(Event):
     def __init__(self, thing, data):
         Event.__init__(self, thing, "overheated", data=data)
-
-
-class FadeAction(Action):
-    def __init__(self, thing, input_):
-        Action.__init__(self, uuid.uuid4().hex, thing, "fade", input_=input_)
-
-    def perform_action(self):
-        time.sleep(self.input["duration"] / 1000)
-        self.thing.set_property("brightness", self.input["brightness"])
-        self.thing.add_event(OverheatedEvent(self.thing, 102))
 
 
 def make_thing():
@@ -28,44 +19,63 @@ def make_thing():
         "A web connected lamp",
     )
 
-    thing.add_property(
-        Property(
-            thing,
-            "on",
-            Value(True),
-            metadata={
-                "@type": "OnOffProperty",
-                "title": "On/Off",
-                "type": "boolean",
-                "description": "Whether the lamp is turned on",
-            },
-        )
-    )
-    thing.add_property(
-        Property(
-            thing,
-            "brightness",
-            Value(50),
-            metadata={
-                "@type": "BrightnessProperty",
-                "title": "Brightness",
-                "type": "integer",
-                "description": "The level of light from 0-100",
-                "minimum": 0,
-                "maximum": 100,
-                "unit": "percent",
-            },
-        )
+    on_property = Property(
+        thing,
+        "on",
+        initial_value=True,
+        metadata={
+            "@type": "OnOffProperty",
+            "title": "On/Off",
+            "type": "boolean",
+            "description": "Whether the lamp is turned on",
+        },
     )
 
-    thing.add_available_action(
+    brightness_property = Property(
+        thing,
+        "brightness",
+        initial_value=50,
+        metadata={
+            "@type": "BrightnessProperty",
+            "title": "Brightness",
+            "type": "integer",
+            "description": "The level of light from 0-100",
+            "minimum": 0,
+            "maximum": 100,
+            "unit": "percent",
+        },
+    )
+
+    thing.add_property(on_property)
+    thing.add_property(brightness_property)
+
+    async def async_fade_function(args):
+        print("Starting fade function")
+        await asyncio.sleep(args["duration"] / 1000)
+        brightness_property.set_value(args["brightness"])
+        print("Finished fade function")
+        return "Return value"
+
+    def fade_function(args):
+        print("Starting fade function")
+        time.sleep(args["duration"] / 1000)
+        brightness_property.set_value(args["brightness"])
+        print("Finished fade function")
+        return "Return value"
+
+    fade_action = Action(
+        thing,
         "fade",
-        {
+        invokeaction=async_fade_function,
+        metadata={
             "title": "Fade",
             "description": "Fade the lamp to a given level",
             "input": {
                 "type": "object",
-                "required": ["brightness", "duration",],
+                "required": [
+                    "brightness",
+                    "duration",
+                ],
                 "properties": {
                     "brightness": {
                         "type": "integer",
@@ -81,8 +91,9 @@ def make_thing():
                 },
             },
         },
-        FadeAction,
     )
+
+    thing.add_action(fade_action)
 
     thing.add_available_event(
         "overheated",
@@ -101,7 +112,7 @@ def run_server():
 
     # If adding more than one thing, use MultipleThings() with a name.
     # In the single thing case, the thing's name will be broadcast.
-    server = WebThingServer(SingleThing(thing), port=8888)
+    server = WebThingServer(thing, port=8888, debug=True)
     try:
         logging.info("starting the server")
         server.start()
